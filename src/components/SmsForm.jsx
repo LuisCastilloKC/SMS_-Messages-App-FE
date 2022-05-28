40483fb6045ab18747be6ed9e8fdd8d30ab94918
Luis Castillo <mr.luiscastillo@gmail.com>
2022-05-27 23:58:14 -0400

    add fetch POST mobile number and message

--- src/components/SmsForm.jsx
+++ src/components/SmsForm.jsx
@@ -4,6 +4,25 @@ const SmsForm = () => {
     const [ sms, setSms] = useState('')
     const [number, setNumber]= useState('')
 
+    const sendSMs = (e) => {
+       e.preventDefault();
+
+       let smsData = {
+           mobile_number: '1'+ number,
+           message: sms
+       }
+       fetch('http://localhost:3000/sms_messages',{
+       method: 'POST',
+       headers: {
+           'content-type': 'application/json',
+           'accepts': 'application/json'
+            },
+            body: JSON.stringify(smsData)
+        })
+        .then(resp => resp.json())
+        .then(data => console.log(data))
+    }
+
     return(
         <div className='container'>
             <h3>Send SMS Message!</h3>
